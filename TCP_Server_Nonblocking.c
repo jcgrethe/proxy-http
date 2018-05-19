f03eafb1d1db19985eac555cd65adaa2e517774b
juan <jgrethe@itba.edu.ar>
2018-05-19 18:50:50 -0300

    test2

--- TCP_Server_Nonblocking.c
+++ TCP_Server_Nonblocking.c
@@ -33,8 +33,8 @@ int main(int args, char* argv[]){
    int    active_socket, max_sd, new_sd, desc_ready;
    char   buffer[1025];
    struct sockaddr_in   addr;
-   struct fd_set        master_set, working_set;
-   map_str_t map;
+   fd_set        master_set, working_set;
+   map_void_t map;
    map_init(&map);
    /* Creating an Active Socket to listen requests.*/
    active_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -141,21 +141,24 @@ int main(int args, char* argv[]){
                      break;
                   }
 
-                  if((pipeStruct=map_get(&map,i))!=NULL){
-                     writeToChild(pipeStruct,buffer);
-                  }else{
+                  //if((pipeStruct=map_get(&map,i))!=NULL){
+                    // printf("2\n");
+                    // writeToChild(pipeStruct,buffer);
+                  //}else{
                      pipeStruct=malloc(sizeof(struct pipe));
                      if (pipe(pipeStruct->pipeToChild) == -1 || pipe(pipeStruct->pipeFromChild) == -1) {
                           perror("Could not build pipe.");
                           exit(1);
                       }
-                     map_set(&map,i,pipeStruct);
+                     char str[12];
+                     sprintf(str,"%d",i);
+                     map_set(&map,str,pipeStruct);
                      // Close unused ends of pipes
                      int pid = fork();
                      if(pid == 0){
-                        close(stdinFD);
-                        close(stdoutFD);
-                        stdinFD = stdoutFD = -1;
+                        //close(stdinFD);
+                        //close(stdoutFD);
+                        //stdinFD = stdoutFD = -1;                                                
                         close(pipeStruct->pipeToChild[1]);
                         close(pipeStruct->pipeFromChild[0]);
                         pipeStruct->pipeToChild[1] = pipeStruct->pipeFromChild[0] = -1;
@@ -166,7 +169,7 @@ int main(int args, char* argv[]){
                         pipeStruct->pipeToChild[0] = pipeStruct->pipeFromChild[1] = -1;
                         writeToChild(pipeStruct,buffer);
                      } 
-                  }   
+                 // }   
                }while(rc>0 && total<1024*8);
             } 
          } 
@@ -180,29 +183,28 @@ void writeToChild(struct pipe * pipeStruct,char * buffer){
 }
 
 int handleFirstConnection(char* buffer){
-   int    socket_w;
-   struct sockaddr_in   addr;
-   struct fd_set        master_set, working_set;
-   /* Creating an Active Socket to listen requests.*/
-   socket_w = socket(AF_INET , SOCK_STREAM , 0);
-   if (socket < 0){
-      perror("Error opening active socket.\n");
-      exit(EXIT_FAILURE);
-   }
-   if(inet_pton(AF_INET, buffer, &addr.sin_addr)<=0)
-    {
-        printf("\n inet_pton error occured\n");
-        return 1;
-    } 
-    if(connect(socket_w,&addr, sizeof(addr)) < 0)
-    {
-       printf("\n Error : Connect Failed \n");
-       return 1;
-    } 
-    if( send(socket_w, "hola" , 5 , 0) < 0){
-            puts("Send failed");
-            return 1;
+   int sockfd, numbytes;  
+        struct hostent *he;
+        struct sockaddr_in their_addr; /* connector's address information */
+   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+            perror("socket");
+            exit(1);
+        }
+        printf("%s\n",buffer);
+        their_addr.sin_addr.s_addr = inet_addr("192.168.0.150");
+        their_addr.sin_family = AF_INET;      /* host byte order */
+        their_addr.sin_port = htons(10000);    /* short, network byte order */
+        bzero(&(their_addr.sin_zero), 8);     /* zero the rest of the struct */
+
+     if (connect(sockfd, (struct sockaddr *)&their_addr, \
+                                              sizeof(struct sockaddr)) == -1) {
+            perror("connect");
+            exit(1);
+        }
+      printf("pasa\n");
+    if (send(sockfd, "Hello, world!\n", 14, 0) == -1){
+                      perror("send");
+            exit (1);
       }
-  
 }
 
