65d7180449217578b1bc4b5503f89437358665ea
j1nma <j1nma@users.noreply.github.com>
2018-06-02 21:15:55 -0300

    commented sctp from macos development with HTTP proxy

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /**
- * main.c - servidor proxy socks concurrente
+ * main.c - servidor proxy http y sctp concurrente
  *
  * Interpreta los argumentos de línea de comandos, y monta un socket
  * pasivo.
@@ -27,13 +27,13 @@
 #include "selector.h"
 #include "httpnio.h"
 
-#include "sctp/sctp_integration.h"
+//#include "sctp/sctp_integration.h"
 
 static bool done = false;
 
 static void
 sigterm_handler(const int signal) {
-    printf("signal %d, cleaning up and exiting\n",signal);
+    printf("signal %d, cleaning up and exiting\n", signal);
     done = true;
 }
 
@@ -42,15 +42,15 @@ main(const int argc, const char **argv) {
     unsigned port = 1080;
     unsigned sctp_port = 1081;
 
-    if(argc == 1) {
+    if (argc == 1) {
         // utilizamos el default
-    } else if(argc == 2) {
-        char *end     = 0;
+    } else if (argc == 2) {
+        char *end = 0;
         const long sl = strtol(argv[1], &end, 10);
 
-        if (end == argv[1]|| '\0' != *end 
-           || ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno)
-           || sl < 0 || sl > USHRT_MAX) {
+        if (end == argv[1] || '\0' != *end
+            || ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno)
+            || sl < 0 || sl > USHRT_MAX) {
             fprintf(stderr, "port should be an integer: %s\n", argv[1]);
             return 1;
         }
@@ -63,41 +63,42 @@ main(const int argc, const char **argv) {
     // no tenemos nada que leer de stdin
     close(0);
 
-    const char       *err_msg = NULL;
-    selector_status   ss      = SELECTOR_SUCCESS;
-    fd_selector selector      = NULL;
+    const char *err_msg = NULL;
+    selector_status ss = SELECTOR_SUCCESS;
+    fd_selector selector = NULL;
 
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
-    addr.sin_family      = AF_INET;
+    addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port        = htons(port);
+    addr.sin_port = htons(port);
 
     struct sockaddr_in addr_sctp;
     memset(&addr_sctp, 0, sizeof(addr_sctp));
-    addr_sctp.sin_family      = AF_INET;
+    addr_sctp.sin_family = AF_INET;
     addr_sctp.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr_sctp.sin_port        = htons(sctp_port);
+    addr_sctp.sin_port = htons(sctp_port);
 
 
     const int server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    const int sctp_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
+//    const int sctp_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
 
-    if(server < 0) {
+    if (server < 0) {
         err_msg = "unable to create server socket";
         goto finally;
     }
-    if(sctp_socket < 0) {
-        err_msg = "unable to create sctp socket";
-        goto finally;
-    }
-    fprintf(stdout, "Listening on TCP port %d for http and %d for sctp.\n", port, sctp_port);
+//    if (sctp_socket < 0) {
+//        err_msg = "unable to create sctp socket";
+//        goto finally;
+//    }
+//    fprintf(stdout, "Listening on TCP port %d for http and %d for sctp.\n", port, sctp_port);
+    fprintf(stdout, "Listening on TCP port %d for http.\n", port);
 
     // man 7 ip. no importa reportar nada si falla.
-    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int) {1}, sizeof(int));
 
     // Main proxy socket listening
-    if(bind(server, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+    if (bind(server, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
         err_msg = "unable to bind socket";
         goto finally;
     }
@@ -109,101 +110,99 @@ main(const int argc, const char **argv) {
 
 
     //SCTP Socket listening
-    if(bind(sctp_socket, (struct sockaddr*) &addr_sctp, sizeof(addr_sctp)) < 0) {
-        err_msg = "unable to bind socket for sctp";
-        goto finally;
-    }
-
-    if (listen(sctp_socket, 20) < 0) {
-        err_msg = "unable to listen sctp socket";
-        goto finally;
-    }
+//    if (bind(sctp_socket, (struct sockaddr *) &addr_sctp, sizeof(addr_sctp)) < 0) {
+//        err_msg = "unable to bind socket for sctp";
+//        goto finally;
+//    }
+//
+//    if (listen(sctp_socket, 20) < 0) {
+//        err_msg = "unable to listen sctp socket";
+//        goto finally;
+//    }
     // check with: sudo nmap -sY localhost -PY -p 1081
 
     // registrar sigterm es útil para terminar el programa normalmente.
     // esto ayuda mucho en herramientas como valgrind.
     signal(SIGTERM, sigterm_handler);
-    signal(SIGINT,  sigterm_handler);
+    signal(SIGINT, sigterm_handler);
 
-    if(selector_fd_set_nio(server) == -1) {
+    if (selector_fd_set_nio(server) == -1) {
         err_msg = "getting server socket flags";
         goto finally;
     }
     const struct selector_init conf = {
-        .signal = SIGALRM,
-        .select_timeout = {
-            .tv_sec  = 10,
-            .tv_nsec = 0,
-        },
+            .signal = SIGALRM,
+            .select_timeout = {
+                    .tv_sec  = 10,
+                    .tv_nsec = 0,
+            },
     };
-    if(0 != selector_init(&conf)) {
+    if (0 != selector_init(&conf)) {
         err_msg = "initializing selector";
         goto finally;
     }
 
     selector = selector_new(1024);
-    if(selector == NULL) {
+    if (selector == NULL) {
         err_msg = "unable to create selector";
         goto finally;
     }
     const struct fd_handler http = {
-        .handle_read       = http_passive_accept,
-        .handle_write      = NULL,
-        .handle_close      = NULL, // nada que liberar
+            .handle_read       = http_passive_accept,
+            .handle_write      = NULL,
+            .handle_close      = NULL, // nada que liberar
     };
-        const struct fd_handler sctp_handler = {
-        .handle_read       = sctp_passive_accept,  //TODO: Put sctp function
-        .handle_write      = NULL,
-        .handle_close      = NULL, // nada que liberar
-    };
+//    const struct fd_handler sctp_handler = {
+//            .handle_read       = sctp_passive_accept,  //TODO: Put sctp function
+//            .handle_write      = NULL,
+//            .handle_close      = NULL, // nada que liberar
+//    };
     ss = selector_register(selector, server, &http,
-                                              OP_READ, NULL);
-    selector_status ss_sctp = selector_register(selector, sctp_socket, &sctp_handler,
-                                              OP_READ, NULL);
-    if(ss != SELECTOR_SUCCESS) {
+                           OP_READ, NULL);
+//    selector_status ss_sctp = selector_register(selector, sctp_socket, &sctp_handler,
+//                                                OP_READ, NULL);
+    if (ss != SELECTOR_SUCCESS) {
         err_msg = "registering server fd";
         goto finally;
     }
-    if(ss_sctp != SELECTOR_SUCCESS) {
-        err_msg = "registering sctp fd";
-        goto finally;
-    }
+//    if (ss_sctp != SELECTOR_SUCCESS) {
+//        err_msg = "registering sctp fd";
+//        goto finally;
+//    }
 
 
-
-
-    for(;!done;) {
+    for (; !done;) {
         err_msg = NULL;
         ss = selector_select(selector);
-        if(ss != SELECTOR_SUCCESS) {
+        if (ss != SELECTOR_SUCCESS) {
             err_msg = "serving";
             goto finally;
         }
     }
-    if(err_msg == NULL) {
+    if (err_msg == NULL) {
         err_msg = "closing";
     }
 
     int ret = 0;
-finally:
-    if(ss != SELECTOR_SUCCESS) {
-        fprintf(stderr, "%s: %s\n", (err_msg == NULL) ? "": err_msg,
-                                  ss == SELECTOR_IO
-                                      ? strerror(errno)
-                                      : selector_error(ss));
+    finally:
+    if (ss != SELECTOR_SUCCESS) {
+        fprintf(stderr, "%s: %s\n", (err_msg == NULL) ? "" : err_msg,
+                ss == SELECTOR_IO
+                ? strerror(errno)
+                : selector_error(ss));
         ret = 2;
-    } else if(err_msg) {
+    } else if (err_msg) {
         perror(err_msg);
         ret = 1;
     }
-    if(selector != NULL) {
+    if (selector != NULL) {
         selector_destroy(selector);
     }
     selector_close();
 
     http_pool_destroy();
 
-    if(server >= 0) {
+    if (server >= 0) {
         close(server);
     }
     return ret;
