0ac67ed796589170d6c88d1f17f316d61f0335d2
juan <jgrethe@itba.edu.ar>
2018-06-12 05:47:47 -0300

    post big

--- httpnio.c
+++ httpnio.c
@@ -844,19 +844,14 @@ request_write(struct selector_key *key) {
                     ret = RESPONSE;
                     selector_set_interest_key(key, OP_READ);
                 } else {
-                    ret = COPY;
 
-                    selector_set_interest_key(key, OP_NOOP);
                     if(buffer_can_read(d->rb)) {
-
                         selector_set_interest(key->s, *d->origin_fd, OP_WRITE);
-
                     } else {
-
                         selector_set_interest(key->s, *d->client_fd, OP_READ);
-
+                        selector_set_interest(key->s, *d->origin_fd, OP_READ);
                     }
-
+                    ret = COPY;
 
                 }
 //                ret = COPY;
@@ -882,84 +877,118 @@ request_write(struct selector_key *key) {
 ////////////////////////////////////////////////////////////////////////////////
 // COPY
 ////////////////////////////////////////////////////////////////////////////////
-
 static void
 copy_init(const unsigned state, struct selector_key *key) {
-    struct request_st *d = &ATTACHMENT(key)->client.request;
+    struct copy * d = &ATTACHMENT(key)->client.copy;
 
-    d->client_fd = &ATTACHMENT(key)->client_fd;
-    d->origin_fd = &ATTACHMENT(key)->origin_fd;
-    d->rb = &(ATTACHMENT(key)->read_buffer);
-    d->wb = &(ATTACHMENT(key)->write_buffer);
+    d->fd        = &ATTACHMENT(key)->client_fd;
+    d->rb        = &ATTACHMENT(key)->read_buffer;
+    d->wb        = &ATTACHMENT(key)->write_buffer;
+    d->duplex    = OP_READ | OP_WRITE;
+    d->other     = &ATTACHMENT(key)->orig.copy;
+
+    d = &ATTACHMENT(key)->orig.copy;
+    d->fd       = &ATTACHMENT(key)->origin_fd;
+    d->rb       = &ATTACHMENT(key)->write_buffer;
+    d->wb       = &ATTACHMENT(key)->read_buffer;
+    d->duplex   = OP_READ | OP_WRITE;
+    d->other    = &ATTACHMENT(key)->client.copy;
+}
+
+/**
+ * Computa los intereses en base a la disponiblidad de los buffer.
+ * La variable duplex nos permite saber si alguna vía ya fue cerrada.
+ * Arrancá OP_READ | OP_WRITE.
+ */
+static fd_interest
+copy_compute_interests(fd_selector s, struct copy* d) {
+    fd_interest ret = OP_NOOP;
+    if ((d->duplex & OP_READ)  && buffer_can_write(d->rb)) {
+        ret |= OP_READ;
+    }
+    if ((d->duplex & OP_WRITE) && buffer_can_read (d->wb)) {
+        ret |= OP_WRITE;
+    }
+    if(SELECTOR_SUCCESS != selector_set_interest(s, *d->fd, ret)) {
+        abort();
+    }
+    return ret;
+}
+
+/** elige la estructura de copia correcta de cada fd (origin o client) */
+static struct copy *
+copy_ptr(struct selector_key *key) {
+    struct copy * d = &ATTACHMENT(key)->client.copy;
+
+    if(*d->fd == key->fd) {
+        // ok
+    } else {
+        d = d->other;
+    }
+    return  d;
 }
 
 /** lee bytes de un socket y los encola para ser escritos en otro socket */
 static unsigned
 copy_r(struct selector_key *key) {
-    struct request_st *d = &ATTACHMENT(key)->client.request;
+    struct copy * d = copy_ptr(key);
 
-    buffer *b = d->wb;
-    uint8_t *ptr;
-    size_t count;
+    assert(*d->fd == key->fd);
+
+    size_t size;
     ssize_t n;
+    buffer* b    = d->rb;
+    unsigned ret = COPY;
 
-    ptr = buffer_write_ptr(b, &count);
-    n = recv(key->fd, ptr, count, 0);
-    if (n > 0) {
-        buffer_write_adv(b, n);
-        selector_set_interest(key->s, *d->origin_fd, OP_WRITE);
-        selector_set_interest(key->s, *d->client_fd, OP_NOOP);
+    uint8_t *ptr = buffer_write_ptr(b, &size);
+    n = recv(key->fd, ptr, size, 0);
+    if(n <= 0) {
+        shutdown(*d->fd, SHUT_RD);
+        d->duplex &= ~OP_READ;
+        if(*d->other->fd != -1) {
+            shutdown(*d->other->fd, SHUT_WR);
+            d->other->duplex &= ~OP_WRITE;
+        }
     } else {
-        return ERROR;
+        buffer_write_adv(b, n);
     }
-    return COPY;
+    copy_compute_interests(key->s, d);
+    copy_compute_interests(key->s, d->other);
+    if(d->duplex == OP_NOOP) {
+        ret = DONE;
+    }
+    return ret;
 }
-//    uint8_t *ptr = buffer_write_ptr(b, &size);
-//    n = recv(key->fd, ptr, size, 0);
-//    if (n <= 0) {
-////        shutdown(*d->fd, SHUT_RD);
-////        d->duplex &= ~OP_READ;
-////        if (*d->other->fd != -1) {
-////            shutdown(*d->other->fd, SHUT_WR);
-////            d->other->duplex &= ~OP_WRITE;
-////        }
-//    } else {
-//        buffer_write_adv(b, n);
-//
-//
-//    }
-//    copy_compute_interests(key->s, d);
-//    copy_compute_interests(key->s, d->other);
-////    selector_set_interest(key->s, ATTACHMENT(key)->origin_fd, OP_READ);
-//    if (d->duplex == OP_NOOP) {
-//        ret = DONE;
-//    }
-//    return ret;
-//}
 
 /** escribe bytes encolados */
 static unsigned
 copy_w(struct selector_key *key) {
-    struct request_st *d = &ATTACHMENT(key)->client.request;
-    buffer *b = d->rb;
-    uint8_t *ptr;
-    size_t count;
-    ssize_t n;
+    struct copy * d = copy_ptr(key);
 
-    ptr = buffer_read_ptr(b, &count);
-    n = send(key->fd, ptr, count, MSG_NOSIGNAL);
-    buffer_read_adv(b, n);
-    if (n == -1) {
-        return ERROR;
-    } else {
-        d->request.content_length = d->request.content_length - n;
-        if (d->request.content_length <= 0) {
-            return RESPONSE;
+    assert(*d->fd == key->fd);
+    size_t size;
+    ssize_t n;
+    buffer* b = d->wb;
+    unsigned ret = COPY;
+
+    uint8_t *ptr = buffer_read_ptr(b, &size);
+    n = send(key->fd, ptr, size, MSG_NOSIGNAL);
+    if(n == -1) {
+        shutdown(*d->fd, SHUT_WR);
+        d->duplex &= ~OP_WRITE;
+        if(*d->other->fd != -1) {
+            shutdown(*d->other->fd, SHUT_RD);
+            d->other->duplex &= ~OP_READ;
         }
+    } else {
+        buffer_read_adv(b, n);
     }
-    selector_set_interest(key->s, *d->origin_fd, OP_NOOP);
-    selector_set_interest(key->s, *d->client_fd, OP_READ);
-    return COPY;
+    copy_compute_interests(key->s, d);
+    copy_compute_interests(key->s, d->other);
+    if(d->duplex == OP_NOOP) {
+        ret = DONE;
+    }
+    return ret;
 }
 //    struct copy *d = copy_ptr(key);
 //
